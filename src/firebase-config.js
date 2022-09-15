29f656f6e5016498171bf8a40dcffa0c3274e71e
SKURTZ <108894925+skurtz0@users.noreply.github.com>
2022-09-15 20:45:27 +0300

    Update firebase-config.js

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from '@firebase/firestore';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDFqhygCzU31LmFJh_3CVjZTJOV9SCd_2c",
-    authDomain: "crud-20883.firebaseapp.com",
-    projectId: "crud-20883",
-    storageBucket: "crud-20883.appspot.com",
-    messagingSenderId: "329443256262",
-    appId: "1:329443256262:web:35b7349d19d22e1f24b4c7"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
   };
 
   const app = initializeApp(firebaseConfig);
